4a069e00ffdd850d01535883f4729803a528907c
yawuxi <ryliov.work@gmail.com>
2024-05-01 22:31:54 +0200

    wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-void setup() {
-  Serial.begin(152600);
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("HOME_2.0", "49298080");
+  Serial.println("\nConnecting...");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(100);
+  }
+
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  delay(500);
-  Serial.println("HELLO");
+void loop()
+{
 }
-
